8bb549e3e74b1ea364ac215586fcc0b64a1dffb3
ermrk <ermrk@seznam.cz>
2016-06-28 14:47:07 +0200

    Client doesn't drop connection

--- client/TestClient.py
+++ client/TestClient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python           # This is client.py file
 
-import socket               # Import socket module
+import socket  # Import socket module
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 9999                # Reserve a port for your service.
-
-s.connect(("localhost", port))
 while True:
     direction = input("Where do you want to go?")
+    s = socket.socket()  # Create a socket object
+    host = socket.gethostname()  # Get local machine name
+    port = 9999  # Reserve a port for your service.
+    s.connect(("localhost", port))
     s.send(direction.encode("utf-8"))
-s.close                     # Close the socket when done
+    received = s.recv(1024)
+    s.close  # Close the socket when done
